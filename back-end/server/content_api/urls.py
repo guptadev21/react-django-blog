a6e4d7fc236c5fd3afdc07ad9c6d797924f73864
guptadev21 <guptadev.2102@gmail.com>
2024-01-29 18:16:30 +0530

    added delete feature for blog but not working

--- back-end/server/content_api/urls.py
+++ back-end/server/content_api/urls.py
@@ -1,12 +1,24 @@
+# from django.urls import include, path
+# from .views import blogsViewSet, BlogDestroyAPIView
+# from rest_framework import routers
+
+# router = routers.DefaultRouter()
+# router.register(r'blogs', blogsViewSet)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('delete/<str:title>/', BlogDestroyAPIView.as_view(), name='blog-delete'),
+# ]
+
+
 from django.urls import include, path
-from .views import *
+from .views import BlogsViewSet, BlogDestroyAPIView
 from rest_framework import routers
 
-
 router = routers.DefaultRouter()
-router.register(r'blogs', blogsViewSet)
+router.register(r'blogs', BlogsViewSet)
 
 urlpatterns = [
-    # Add your URL patterns here
     path('', include(router.urls)),
+    path('blogs/delete/', BlogDestroyAPIView.as_view(), name='blog-delete'),
 ]
